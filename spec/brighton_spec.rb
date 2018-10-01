863b7321c808f08a08368f60f62e10bb32e1f059
Jez Nicholson <jez.nicholson@gmail.com>
2018-10-01 14:18:22 +0100

    added a few LAs and use VCR

---
+++ spec/brighton_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe UKPlanningScraper::Authority do
+
+  describe '#named' do
+
+    let(:scraper) { UKPlanningScraper::Authority.named(authority_name) }
+
+    context 'for 4 days of Brighton and Hove' do
+      let(:authority_name) { 'Brighton and Hove' }
+
+      it 'returns apps' do
+        apps = VCR.use_cassette("#{self.class.description}") {
+          scraper.scrape({ decided_days: 4 })
+        }
+        pp apps
+#        expect(authority).to be_a(UKPlanningScraper::Authority)
+      end
+    end
+
+  end
+
+end
