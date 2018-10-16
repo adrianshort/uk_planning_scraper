a24582ee030d06c05e10643216d41849f54d78dc
Adrian Short <adrian@adrianshort.org>
2018-10-16 11:39:49 +0100

    Add Northern Ireland multi-authority site #31

--- spec/authority_spec.rb
+++ spec/authority_spec.rb
@@ -28,7 +28,7 @@ describe UKPlanningScraper::Authority do
     let(:all) { described_class.all }
 
     it 'returns all authorities' do
-      expect(all.count).to eq(74)
+      expect(all.count).to eq(75)
     end
 
     it 'returns a list of authorities' do
